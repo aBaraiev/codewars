64f9da3a6c9949d94c70a544457c7d8ca23f4411
aBaraiev <abaraiev@gmail.com>
2022-02-08 12:45:35 +0200

    +1 x 6kuy

--- 5kuy/bananas/bananas.js
+++ 5kuy/bananas/bananas.js
@@ -1,29 +1,29 @@
-// Kata Task
-// Given a string of letters a, b, n how many different ways can you make the word "banana"
-// by crossing out various letters and then reading left-to-right?
-// (Use - to indicate a crossed-out letter)
-//
-// Example
-// Input
-//
-// bbananana
-// Output
-//
-// b-anana--
-// b-anan--a
-// b-ana--na
-// b-an--ana
-// b-a--nana
-// b---anana
-// -banana--
-// -banan--a
-// -bana--na
-// -ban--ana
-// -ba--nana
-// -b--anana
-// Notes
-// You must return all possible bananas, but the order does not matter
-// The example output above is formatted for readability. Please refer to the example tests for expected format of your result.
+// // Kata Task
+// // Given a string of letters a, b, n how many different ways can you make the word "banana"
+// // by crossing out various letters and then reading left-to-right?
+// // (Use - to indicate a crossed-out letter)
+// //
+// // Example
+// // Input
+// //
+// // bbananana
+// // Output
+// //
+// // b-anana--
+// // b-anan--a
+// // b-ana--na
+// // b-an--ana
+// // b-a--nana
+// // b---anana
+// // -banana--
+// // -banan--a
+// // -bana--na
+// // -ban--ana
+// // -ba--nana
+// // -b--anana
+// // Notes
+// // You must return all possible bananas, but the order does not matter
+// // The example output above is formatted for readability. Please refer to the example tests for expected format of your result.
 
 var bananas = function (s) {
     let bananas = [];
@@ -75,4 +75,3 @@ const createTriples = array => {
     }
     return triples;
 }
-
