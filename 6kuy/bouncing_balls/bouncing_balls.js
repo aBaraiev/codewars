285236f23f293004d65795ef282e7932c25558f9
aBaraiev <abaraiev@gmail.com>
2022-02-13 10:45:50 +0200

    +1 x 6kuy

--- 6kuy/bouncing_balls/bouncing_balls.js
+++ 6kuy/bouncing_balls/bouncing_balls.js
@@ -1,5 +1,7 @@
 function bouncingBall(h, bounce, window) {
+    
     if (bounce <= 0 || bounce >= 1 || window >= h || h <= 0) return -1;
+
     let timesSeen = 1;
     while (true) {
         h = bounce * h;
